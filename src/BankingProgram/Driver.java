d9706577dcad25ba28a0d9620129cfb69eeedb3b
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2015-10-14 12:30:37 -0400

    Added a driver class

---
+++ src/BankingProgram/Driver.java
@@ -0,0 +1,10 @@
+package BankingProgram;
+
+/***********************************************************************
+ * Launches the BankingProgram GUI so that the user can interact
+ **********************************************************************/
+public class Driver {
+    public static void main(String[] args){
+        //TODO: Actually launch program
+    }
+}
