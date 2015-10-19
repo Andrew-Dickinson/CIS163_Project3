df54ef44607f6f60db23afd80bfb06d032e8b3d7
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2015-10-18 20:50:23 -0400

    Renamed BankGUI to BankPanel and added TODOs

--- src/edu/gvsu/CIS163/Fall_2015/Andrew_Sully/BankingProgram/BankGUI.java
+++
@@ -1,6 +0,0 @@
-package edu.gvsu.CIS163.Fall_2015.Andrew_Sully.BankingProgram;
-
-//TODO: Javadoc this class
-public class BankGUI {
-
-}
---
+++ src/edu/gvsu/CIS163/Fall_2015/Andrew_Sully/BankingProgram/BankPanel.java
@@ -0,0 +1,13 @@
+package edu.gvsu.CIS163.Fall_2015.Andrew_Sully.BankingProgram;
+
+import javax.swing.*;
+
+/***********************************************************************
+ * Displays the GUI for interacting with the banking program
+ **********************************************************************/
+public class BankPanel extends JFrame {
+    // BankGUI class maintains an instance of BankController.
+    // In the BankGUI class, set the BankModel as the model for the
+    // JList in the BankGUI class using the setModel() method of
+    // the JList class.
+}
