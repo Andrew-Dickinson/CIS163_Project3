4726e14c339628bccd45e10f57f1c7d53efd96b0
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2015-10-19 14:03:17 -0400

    Removed bank controller

--- src/edu/gvsu/CIS163/Fall_2015/Andrew_Sully/BankingProgram/BankPanel.java
+++ src/edu/gvsu/CIS163/Fall_2015/Andrew_Sully/BankingProgram/BankPanel.java
@@ -6,7 +6,6 @@ import javax.swing.*;
  * Displays the GUI for interacting with the banking program
  **********************************************************************/
 public class BankPanel extends JFrame {
-    // BankGUI class maintains an instance of BankController.
     // In the BankGUI class, set the BankModel as the model for the
     // JList in the BankGUI class using the setModel() method of
     // the JList class.
