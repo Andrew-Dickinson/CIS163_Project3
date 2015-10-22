a07fdb99f33e071fe5894aeba2cca97d67bfa68c
justsully0 <sulman7@gmail.com>
2015-10-21 21:17:12 -0400

    Added the rest of the GUI options and added comments

--- src/edu/gvsu/CIS163/Fall_2015/Andrew_Sully/BankingProgram/BankPanel.java
+++ src/edu/gvsu/CIS163/Fall_2015/Andrew_Sully/BankingProgram/BankPanel.java
@@ -15,22 +15,41 @@ import javax.swing.*;
 // the JList class.
 
 public class BankPanel extends JFrame {
+	
 	private JFrame frame; 
 	private JPanel panel;
 	private JMenu file;
 	private JMenu sort;
 	private JMenuItem quit;
+	//save to a Binary File
 	private JMenuItem saveToB;
+	//Load from a Binary FIle
 	private JMenuItem loadFromB;
+	//save to a Text file
 	private JMenuItem saveToT;
+	//load from a text file
 	private JMenuItem loadFromT;
+	//save to a XML document
 	private JMenuItem saveToX;
+	//load from an XML document
 	private JMenuItem loadFromX;
+	//sort by Account number
+	private JMenuItem byAcctNum;
+	//sort by account owner
+	private JMenuItem byAcctOwn;
+	//sort by date opened
+	private JMenuItem byDateOpen;
+	//add a savings account
+	private JMenuItem addCheck;
+	//add a checking account
+	private JMenuItem addSave;
 	private JTable table;
 	private JMenuBar menuBar;
 
 	public BankPanel(){
+		//a new button Listener
 		ButtonListener butListener = new ButtonListener();
+		//Initializes objects 
 		frame = new JFrame("Bank Application"); 
 		menuBar = new JMenuBar();
 		file = new JMenu("File");
@@ -43,8 +62,14 @@ public class BankPanel extends JFrame {
 		loadFromT = new JMenuItem("Load From Txt");
 		saveToX = new JMenuItem("Save To XML");
 		loadFromX = new JMenuItem("Load From XML");
+		byAcctNum = new JMenuItem("By Account Number");
+		byAcctOwn = new JMenuItem("By Account Owner");
+		byDateOpen = new JMenuItem("By Date Opened");
+		addCheck = new JMenuItem("Add a Checking Account");
+		addSave = new JMenuItem("Add a Savings Account");
 		table = new JTable();
 		
+		//adds actionListener butListener to objects
 		quit.addActionListener(butListener);
 		saveToB.addActionListener(butListener);
 		loadFromB.addActionListener(butListener);
@@ -52,7 +77,16 @@ public class BankPanel extends JFrame {
 		loadFromT.addActionListener(butListener);
 		saveToX.addActionListener(butListener);
 		loadFromX.addActionListener(butListener);
+		byAcctNum.addActionListener(butListener);
+		byAcctOwn.addActionListener(butListener);
+		byDateOpen.addActionListener(butListener);
+		addCheck.addActionListener(butListener);
+		addSave.addActionListener(butListener);
 		
+		//formats the file and sort drop-down menus
+		file.add(addCheck);
+		file.add(addSave);
+		file.addSeparator();
 		file.add(saveToB);
 		file.add(loadFromB);
 		file.addSeparator();
@@ -63,11 +97,15 @@ public class BankPanel extends JFrame {
 		file.add(loadFromX);
 		file.addSeparator();
 		file.add(quit);
+		sort.add(byAcctNum);
+		sort.add(byAcctOwn);
+		sort.add(byDateOpen);
 		menuBar.add(file);
 		menuBar.add(sort);
 		
 		panel.add(table);
 		
+		//sets the frame
 		frame.add(panel);
 		frame.setJMenuBar(menuBar);
 		frame.setSize(500,500);
@@ -75,40 +113,74 @@ public class BankPanel extends JFrame {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
+	
 	public static void main(String[] args) {
 		new BankPanel();
 	}
 	
+	
 	private class ButtonListener implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
+			//what happens when a button is pressed
+			//quits the program
 			if(quit == event.getSource()){
 				System.exit(0);
 			}
 			
+			//saves accounts to a binary file
 			if(saveToB == event.getSource()){
 				//TODO: save to binary
 			}
 			
+			//loads accounts from a binary file
 			if(loadFromB == event.getSource()){
 				//TODO: load from binary
 			}
 			
+			//saves accounts to a Text file
 			if(saveToT == event.getSource()){
 				//TODO: save to text
 			}
 			
+			//loads accounts from a text file
 			if(loadFromT == event.getSource()){
 				//TODO: load from text
 			}
 			
+			//saves accounts to a XML file
 			if(saveToX == event.getSource()){
 				//TODO: save to XML
 			}
 			
+			//loads accounts from a XML file
 			if(loadFromX == event.getSource()){
 				//TODO: load from XML
 			}
 			
+			//sorts JTable by account numbers
+			if(byAcctNum == event.getSource()){
+				//TODO: sort by account number
+			}
+			
+			//sorts JTable by account owners names
+			if(byAcctOwn == event.getSource()){
+				//TODO: sort by owner name
+			}
+			
+			//sorts JTable by the accounts date opened
+			if(byDateOpen == event.getSource()){
+				//TODO: sort by date opened
+			}
+			
+			//adds a checking account to the JTable
+			if(addCheck == event.getSource()){
+				//TODO: add a checking account
+			}
+			
+			//adds a savings account to the JTable
+			if(addSave == event.getSource()){
+				//TODO: Add a savings account
+			}
 		}
 	}
 }
