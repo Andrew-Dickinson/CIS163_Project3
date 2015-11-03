7acb28792dc5e8bcaf6bdca21615eb5acf5e9601
justsully0 <sulman7@gmail.com>
2015-11-03 01:54:48 -0500

    javadoc'ed Bank Panel more, moved some objects arround so it flows better

--- src/edu/gvsu/CIS163/Fall_2015/Andrew_Sully/BankingProgram/BankPanel.java
+++ src/edu/gvsu/CIS163/Fall_2015/Andrew_Sully/BankingProgram/BankPanel.java
@@ -75,6 +75,13 @@ public class BankPanel extends JPanel {
     /** Adds an copy of the selected account to the JTable. */
     private JButton cloneAccountButton;
     
+    /** Panel Specifically for the buttons cloneAccountButton,
+     * editAccountButton, removeAccountButton and addAccountButton*/
+    JPanel buttonPanel;
+    
+    /** JLayerdPane on which the JTable table is displayed. */
+    JLayeredPane primaryDisplayArea;
+    
 	/** Constructor of main frame */
 	public BankPanel(JFrame frame){
 	    
@@ -84,6 +91,7 @@ public class BankPanel extends JPanel {
 		/** Creates a new ColumnSortListener*/
 		ColumnSortListener sortListener = new ColumnSortListener();
 		
+		/** Objects are Instantiated. */
 		menuBar = new JMenuBar();
 		file = new JMenu("File");
 		sort = new JMenu("Sort");
@@ -94,21 +102,28 @@ public class BankPanel extends JPanel {
 		byAcctOwn = new JMenuItem("By Account Owner");
 		byDateOpen = new JMenuItem("By Date Opened");
 		addAccountMenuButton = new JMenuItem("Add Account");
-
-		/** Create a new table instance*/
+		buttonPanel = new JPanel();
+		addAccountButton = new JButton("New Account");
+		removeAccountButton = new JButton("Delete Account");
+		cloneAccountButton = new JButton("Clone Account");
+		editAccountButton = new JButton("Edit Account");
+		primaryDisplayArea = new JLayeredPane();
+		
+		/** Create a new table instance. */
 		model = new BankModel();
 		table = new JTable(model);
-        table.setPreferredScrollableViewportSize(new Dimension(900, 400));
+        table.setPreferredScrollableViewportSize
+        	(new Dimension(900, 400));
         table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 
-        // Adds the table to a scrolling pane
+        /** Adds the table to a scrolling pane. */
 		scrollPane = new JScrollPane(table);
         table.setDragEnabled(false);
         table.getTableHeader().setReorderingAllowed(false);
     	table.getTableHeader().addMouseListener(sortListener);
     	table.addMouseListener(new PopClickListener());
 
-		//adds actionListener butListener to objects
+    	/** Adds actionListener butListener to objects. */
 		quit.addActionListener(butListener);
 		save.addActionListener(butListener);
 		load.addActionListener(butListener);
@@ -116,8 +131,12 @@ public class BankPanel extends JPanel {
 		byAcctOwn.addActionListener(butListener);
 		byDateOpen.addActionListener(butListener);
 		addAccountMenuButton.addActionListener(butListener);
-		
-		//formats the file and sort drop-down menus
+		addAccountButton.addActionListener(butListener);
+        removeAccountButton.addActionListener(butListener);
+        editAccountButton.addActionListener(butListener);
+        cloneAccountButton.addActionListener(butListener);
+        
+		/** Formats the file and sort drop-down menus. */
 		file.add(save);
 		file.add(load);
 		file.addSeparator();
@@ -130,29 +149,16 @@ public class BankPanel extends JPanel {
 		menuBar.add(file);
 		menuBar.add(sort);
 
-        JPanel buttonPanel = new JPanel();
-
-        addAccountButton = new JButton("New Account");
-        addAccountButton.addActionListener(butListener);
+		/** Add Buttons to buttonPanel. */
         buttonPanel.add(addAccountButton);
-
-        removeAccountButton = new JButton("Delete Account");
-        removeAccountButton.addActionListener(butListener);
         buttonPanel.add(removeAccountButton);
-
-        editAccountButton = new JButton("Edit Account");
-        editAccountButton.addActionListener(butListener);
         buttonPanel.add(editAccountButton);
-
-        cloneAccountButton = new JButton("Clone Account");
-        cloneAccountButton.addActionListener(butListener);
         buttonPanel.add(cloneAccountButton);
-
-        JLayeredPane primaryDisplayArea = new JLayeredPane();
+        
+        /** Formats primaryDisplayArea. */
         primaryDisplayArea.setPreferredSize(new Dimension(900, 400));
         primaryDisplayArea.add(scrollPane, 0, 0);
         scrollPane.setBounds(0, 0, 900, 400);
-
         nothingTextLabel = new JLabel("No accounts yet...");
         primaryDisplayArea.add(nothingTextLabel, 1, 0);
         nothingTextLabel.setBounds(375, 190, 300, 20);
@@ -174,6 +180,7 @@ public class BankPanel extends JPanel {
             return model.getAccount(row);
         }
     }
+    
 
     /*******************************************************************
      * Displays a warning dialog to the user when they try to use a
@@ -193,16 +200,17 @@ public class BankPanel extends JPanel {
     private void showAddAccountDialog(){
         AccountAddDialog dialog = new AccountAddDialog(getParent());
 
-        //Null if canceled or invalid data
+        /** Null if canceled or invalid data. */
         Account account = dialog.displayDialog();
 
-        //adds account to bank Model
+        /** Adds Account to the bank model. */
         if (account != null) {
             if (!model.hasAccountNumber(account.getNumber())){
-                //The new account's number is unique
+            	/** Account number is unique. */
                 model.addAccount(account);
             } else {
-                //The account number is a duplicate. Alert the user
+            	/** Duplicated Account Number. Let's warn the
+           	    *user. */
                 JOptionPane.showMessageDialog(getParent(),
                         "Accounts must have unique account numbers",
                         "Duplicate Detected",
@@ -214,83 +222,102 @@ public class BankPanel extends JPanel {
 
         checkIfNoAccountTextIsNeeded();
     }
-
+    
+    /*******************************************************************
+     * TODO:
+     ******************************************************************/
     private void checkIfNoAccountTextIsNeeded(){
         nothingTextLabel.setVisible(model.getRowCount() == 0);
     }
     
+    /*******************************************************************
+     *Edits the selected account, by displaying the AccountAddDialog.
+     ******************************************************************/
     public void editAccount(){
     	 Account account = getSelectedAccount();
          if (account != null) {
              AccountAddDialog dialog =
                      new AccountAddDialog(getParent());
 
-             //Null if canceled or invalid data
+             /** Null if canceled or invalid data. */
              Account editedAccount = dialog.displayDialog(account);
 
-             //Updates account in the bank Model
+             /** Updates account in the bank Model. */
              if (editedAccount != null) {
                  try {
                      model.updateAccount(account, editedAccount);
                  } catch (IllegalArgumentException e){
-                     //The account number is a duplicate. Alert the user
+                	 /** Duplicated Account Number. Let's warn the
+                	  *  user. */
                      JOptionPane.showMessageDialog(getParent(),
-                             "Accounts must have unique account numbers",
-                             "Duplicate Detected",
-                             JOptionPane.ERROR_MESSAGE);
+                    	"Accounts must have unique account numbers",
+                    	"Duplicate Detected",
+                        JOptionPane.ERROR_MESSAGE);
                  }
              }
          } else {
-             //No account was selected. Let's warn the user
+        	 /** No account was selected. Let's warn the user. */
              warnNoAccountSelected();
          }
     }
     
+    /*******************************************************************
+     *Clones the selected account, and asks the user for a new Account 
+     *Number.
+     ******************************************************************/
     public void cloneAccount(){
     	Account account = getSelectedAccount();
         if (account != null) {
             String newNumber = JOptionPane.showInputDialog(
-                    getParent(),
-                    "Please enter a new account number: ",
-                    account.getNumber());
+            	getParent(),"Please enter a new account number: ",
+                account.getNumber());
             if (newNumber != null){
                 if (!model.hasAccountNumber(newNumber)) {
                     account.setNumber(newNumber);
                     model.addAccount(account);
                 } else {
                     JOptionPane.showMessageDialog(getParent(),
-                            "Account number must be unique",
-                            "Error while adding account",
-                            JOptionPane.ERROR_MESSAGE);
+                    	"Account number must be unique",
+                        "Error while adding account",
+                        JOptionPane.ERROR_MESSAGE);
                 }
             }
         } else {
-            //No account was selected. Let's warn the user
+        	/** No account was selected. Let's warn the user. */
             warnNoAccountSelected();
         }
     }
     
+    /*******************************************************************
+     *Deletes the selected account.
+     ******************************************************************/
     public void deleteAccount(){
         if (getSelectedAccount() != null) {
             model.removeAccount(getSelectedAccount());
         } else {
-            //No account was selected. Let's warn the user
+            /** No account was selected. Let's warn the user. */
             warnNoAccountSelected();
         }
 
         checkIfNoAccountTextIsNeeded();
     }
 
+    /*******************************************************************
+     *TODO: (sully)
+     ******************************************************************/
 	private class ButtonListener implements ActionListener {
+	
+	    /***************************************************************
+	     *TODO: (sully)
+	     **************************************************************/
 		public void actionPerformed(ActionEvent event) {
-			//what happens when a button is pressed
 
-			//quits the program
+			/** Quits the application. */
 			if(quit == event.getSource()){
 				System.exit(0);
 			}
 			
-			//saves the account data to files
+			/** Saves the accounts to the selected file type. */
 			if(save == event.getSource()){
                 BankFileDialog bfd = new BankFileDialog(getParent());
                 if (bfd.saveDialog(model)
@@ -302,7 +329,7 @@ public class BankPanel extends JPanel {
                 }
 			}
 			
-			//loads account data from files
+			/** Loads the accounts from the selected document. */
 			if(load == event.getSource()){
                 BankFileDialog bfd = new BankFileDialog(getParent());
                 try {
@@ -312,69 +339,70 @@ public class BankPanel extends JPanel {
                         model = bm;
                     }
                 } catch (IOException | IllegalArgumentException e){
-                    //There was a problem with loading the file
                     JOptionPane.showMessageDialog(getParent(),
-                            "An error occurred while loading this file",
-                            "Unknown Error",
-                            JOptionPane.ERROR_MESSAGE);
+                        "An error occurred while loading this file",
+                        "Unknown Error",
+                        JOptionPane.ERROR_MESSAGE);
                 }
-
                 checkIfNoAccountTextIsNeeded();
 			}
 			
-			//sorts JTable by account numbers
-			if(byAcctNum == event.getSource()){
+			/** Sorts JTable by account numbers. */
+			if(byAcctNum == event.getSource())
 				model.sortByAccountNumber();
-			}
 			
-			//sorts JTable by account owners names
-			if(byAcctOwn == event.getSource()){
+			/** Sorts JTable by account owners names. */
+			if(byAcctOwn == event.getSource())
 				model.sortByAccountName();
-			}
 			
-			//sorts JTable by the accounts date opened
-			if(byDateOpen == event.getSource()){
+			/** Sorts JTable by the accounts date opened. */
+			if(byDateOpen == event.getSource())
 				model.sortByDateOpened();
-			}
 			
-			// adds a checking account to the JTable
-			if (addAccountMenuButton == event.getSource()) {
+			/** Adds an account. */
+			if (addAccountMenuButton == event.getSource()) 
                 showAddAccountDialog();
-			}
 
-            if (addAccountButton == event.getSource()){
+			/** Adds an account. */
+            if (addAccountButton == event.getSource())
                 showAddAccountDialog();
-            }
 
-            if (removeAccountButton == event.getSource()){
+            /** Removes the selected account. */
+            if (removeAccountButton == event.getSource())
             	deleteAccount();
-            }
 
-            if (editAccountButton == event.getSource()){
+            /** Edits the selected account. */
+            if (editAccountButton == event.getSource())
                editAccount();
-            }
-
-            if (cloneAccountButton == event.getSource()){
+            
+            /** Clones the selected account. */
+            if (cloneAccountButton == event.getSource())
                 cloneAccount();
-            }            
 		}
 	}
 	
+    /*******************************************************************
+     *TODO:(sully)
+     ******************************************************************/
 	private class ColumnSortListener extends MouseClickListener {
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			int col = table.columnAtPoint(e.getPoint());
   	        HeaderName name = model.getHeaders()[col];
 
-            Comparator<Account> comp = model.getComparatorFromHeader(name);
+            Comparator<Account> comp = 
+            	model.getComparatorFromHeader(name);
 
-            //If comp is null, something is very bad
-            //That should never happen
+            /**If comp is null, something is very badThat should never 
+             * happen*/
             if (comp != null)
                 model.sortByComparator(comp);
 		}
 	}
 	
+    /*******************************************************************
+     *TODO: (Sully)
+     ******************************************************************/
 	private class PopUp extends JPopupMenu {
 		
   	  	JMenuItem editAccount;
@@ -382,6 +410,10 @@ public class BankPanel extends JPanel {
   	  	JMenuItem deleteAccount;
   	  	
   	  	ButtonListener butListener = new ButtonListener();
+  	  	
+  	    /***************************************************************
+  	     *TODO: (Sully)
+  	     **************************************************************/
   	  	public PopUp()
   	  	{
   		  	editAccount = new JMenuItem("Edit Account");
@@ -396,38 +428,40 @@ public class BankPanel extends JPanel {
   	      	deleteAccount.addActionListener(butListener);
 	      	add(deleteAccount);
   	  	}
-  	    
+
+  	    /***************************************************************
+  	     *TODO: (Sully)
+  	     **************************************************************/
   		private class ButtonListener implements ActionListener {
   			public void actionPerformed(ActionEvent event) {
       			if(editAccount == event.getSource())
-      			{
       				editAccount();
-      			}
+      			
       			if(cloneAccount == event.getSource())
-      			{
       				cloneAccount();
-      			}
+      			
       			if(deleteAccount == event.getSource())
-      			{
       				deleteAccount();
-      			}
   			}
   		}
-  	}
-	  
-	  public class PopClickListener extends MouseAdapter {
-  	    public void mousePressed(MouseEvent e){
-  	        if (e.isPopupTrigger()){
-  	  	        PopUp menu = new PopUp();
-  	  	        menu.show(e.getComponent(), e.getX(), e.getY());
-  	  	    }
-  	    }
+	}
+	
+  	/*******************************************************************
+  	 *TODO: (Sully)
+    ******************************************************************/
+    public class PopClickListener extends MouseAdapter {
+    	public void mousePressed(MouseEvent e){
+    	   if (e.isPopupTrigger()){
+    	  	    PopUp menu = new PopUp();
+    	  	    menu.show(e.getComponent(), e.getX(), e.getY());
+    	  	}
+    	}
 
-  	    public void mouseReleased(MouseEvent e){
-  	        if (e.isPopupTrigger()){
-  	  	        PopUp menu = new PopUp();
-  	  	        menu.show(e.getComponent(), e.getX(), e.getY());
-  	  	    }
-  	    }
-  	}
+    	public void mouseReleased(MouseEvent e){
+    	    if (e.isPopupTrigger()){
+    	  	    PopUp menu = new PopUp();
+    	  	    menu.show(e.getComponent(), e.getX(), e.getY());
+    	   }
+    	}
+    }
 }
