e45f8cfdb33bc5514ff2086fef946e0315ede2f8
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2015-11-02 20:21:44 -0500

    Javadoc'ed BankFileDialog class

--- src/edu/gvsu/CIS163/Fall_2015/Andrew_Sully/BankingProgram/BankFileDialog.java
+++ src/edu/gvsu/CIS163/Fall_2015/Andrew_Sully/BankingProgram/BankFileDialog.java
@@ -7,9 +7,13 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-/**
- * Created by Andrew on 10/27/15.
- */
+/***********************************************************************
+ * A custom implementation of JFileChooser for the purpose of having a
+ * drop-down with multiple custom file filters and having the parsing be
+ * called by methods in this class. Will spit out an appropriate
+ * BankModel based on a selected file or will accept a BankModel and
+ * output it to an appropriate file
+ **********************************************************************/
 public class BankFileDialog extends JFileChooser {
     /**
      * The parent JPanel for this dialog
@@ -26,6 +30,10 @@ public class BankFileDialog extends JFileChooser {
     FileNameExtensionFilter xmlFilter = new FileNameExtensionFilter(
             "XML files", "xml");
 
+    /*******************************************************************
+     * Construct (but do not display) the custom file dialog
+     * @param parent The parent component to this dialog
+     ******************************************************************/
     public BankFileDialog(Component parent){
         this.parent = parent;
         super.setAcceptAllFileFilterUsed(false);
@@ -38,16 +46,26 @@ public class BankFileDialog extends JFileChooser {
      * Display a file choosing dialog for the purpose of selecting a
      * BankModel to import
      * @return The imported BankModel or null if canceled
+     * @throws IOException If there's a problem reading the file
+     * @throws IllegalArgumentException if the file's in the wrong format
      ******************************************************************/
     public BankModel openDialog() throws IOException {
+        //Display and capture the status of the superclass dialog
         int status = super.showOpenDialog(parent);
 
+        //If the dialog exited smoothly
         if (status == JFileChooser.APPROVE_OPTION) {
-            File fileToSave = super.getSelectedFile();
+            //Get the file to open from the superclass dialog
+            File fileToOpen = super.getSelectedFile();
             FileFilter extFilter = super.getFileFilter();
-            String filePath = "" + fileToSave.getAbsolutePath();
+            String filePath = "" + fileToOpen.getAbsolutePath();
 
+            //Figure out what type of file the user selected
             if (extFilter.equals(txtFilter)) {
+                //It was a txt file
+
+                //Automatically append the file extension if not done
+                //by the user
                 if (!filePath.endsWith(txtFilter.getExtensions()[0])) {
                     filePath += txtFilter.getExtensions()[0];
                 }
@@ -57,6 +75,10 @@ public class BankFileDialog extends JFileChooser {
                 bm.loadFromTextFile(filePath);
                 return bm;
             } else if (extFilter.equals(bnkFilter)) {
+                //It was a binary file
+
+                //Automatically append the file extension if not done
+                //by the user
                 if (!filePath.endsWith(bnkFilter.getExtensions()[0])) {
                     filePath += bnkFilter.getExtensions()[0];
                 }
@@ -66,6 +88,10 @@ public class BankFileDialog extends JFileChooser {
                 bm.loadFromBinaryFile(filePath);
                 return bm;
             } else if (extFilter.equals(xmlFilter)) {
+                //It was an XML file
+
+                //Automatically append the file extension if not done
+                //by the user
                 if (!filePath.endsWith(xmlFilter.getExtensions()[0])) {
                     filePath += xmlFilter.getExtensions()[0];
                 }
@@ -88,27 +114,42 @@ public class BankFileDialog extends JFileChooser {
      * @return The terminating status of the dialog
      ******************************************************************/
     public DialogStatus saveDialog(BankModel bm) {
+        //Display and capture the status of the superclass dialog
         int status = super.showSaveDialog(parent);
 
+        //If the dialog exited smoothly
         if (status == JFileChooser.APPROVE_OPTION) {
+            //Get the location to save to from the superclass dialog
             File fileToSave = super.getSelectedFile();
             FileFilter extFilter = super.getFileFilter();
             String filePath = "" + fileToSave.getAbsolutePath();
 
             try {
                 if (extFilter.equals(txtFilter)) {
+                    //It is a txt file
+
+                    //Automatically append the file extension if not done
+                    //by the user
                     if (!filePath.endsWith("." + txtFilter.getExtensions()[0])) {
                         filePath += "." + txtFilter.getExtensions()[0];
                     }
 
                     bm.saveToTextFile(filePath);
                 } else if (extFilter.equals(bnkFilter)) {
+                    //It is a binary file
+
+                    //Automatically append the file extension if not done
+                    //by the user
                     if (!filePath.endsWith("." + bnkFilter.getExtensions()[0])) {
                         filePath += "." + bnkFilter.getExtensions()[0];
                     }
 
                     bm.saveToBinaryFile(filePath);
                 } else if (extFilter.equals(xmlFilter)) {
+                    //It is an XML file
+
+                    //Automatically append the file extension if not done
+                    //by the user
                     if (!filePath.endsWith("." + xmlFilter.getExtensions()[0])) {
                         filePath += "." + xmlFilter.getExtensions()[0];
                     }
